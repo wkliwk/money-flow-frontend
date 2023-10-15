8129fb51cde2769f2075a41232f64c9fa6850bfb
wkliwk <wkliwk@gmail.com>
2023-10-15 20:41:11 +0800

    feat: render the AddExpense component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddExpense from './AddExpense'; // Import the AddExpense component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Money Flow App</h1>
+      <AddExpense /> {/* Render the AddExpense component */}
+      {/* Other components or content can go here */}
     </div>
   );
 }
